1a2a68aa8e6a3f103fdae9f8ac282409ad9e6b87
Colleen <colleenmacklin@gmail.com>
2022-02-18 09:47:29 -0500

    Single Rounds

--- Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
+++ Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
@@ -1,1 +1,1 @@
-namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "2/16/2022 8:27:32 PM";}}
+namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "2/17/2022 8:28:35 PM";}}
